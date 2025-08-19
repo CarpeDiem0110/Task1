781c6392403da721b6f957461476f0d2e194a39e
Ahmet Eren Ceylan <100166488+CarpeDiem0110@users.noreply.github.com>
2025-08-20 00:14:13 +0300

    product show file is added

---
+++ frontend/frontend_app/src/app/products/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export default function ProductsPage() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5233/api/product") // Direkt backend endpoint
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-6">Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
