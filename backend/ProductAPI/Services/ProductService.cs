9b91e23693789ee54eeb8590d2f4da301e0ede01
Ahmet Eren Ceylan <100166488+CarpeDiem0110@users.noreply.github.com>
2025-08-19 17:06:52 +0300

    service is created

---
+++ backend/ProductAPI/Services/ProductService.cs
@@ -0,0 +1,32 @@
+using ProductAPI.DTOs;
+using ProductAPI.Models;
+using ProductAPI.Repositories;
+
+namespace ProductAPI.Services;
+
+public class ProductService
+{
+    private readonly ProductRepository _repository;
+
+    public ProductService(ProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Product>> GetProductsAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Product> AddProductAsync(ProductDTO dto)
+    {
+        var product = new Product
+        {
+            Name = dto.Name,
+            Price = dto.Price
+        };
+        return await _repository.AddAsync(product);
+    }
+}
+
+
