f50b37491a8661f11f59956d7412892667534b55
Ahmet Eren Ceylan <100166488+CarpeDiem0110@users.noreply.github.com>
2025-08-20 00:15:08 +0300

    .

---
+++ frontend/frontend_app/src/app/layout.tsx
@@ -0,0 +1,20 @@
+
+import './global.css'
+
+export const metadata = {
+  title: "Product App",
+  description: "Next.js Product App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-100 text-gray-900">{children}</body>
+    </html>
+  );
+}
+
