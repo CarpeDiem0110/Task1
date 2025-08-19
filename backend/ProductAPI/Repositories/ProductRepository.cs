1c1e069ef160a1a3bfe0e1c8e241a3412875aec0
Ahmet Eren Ceylan <100166488+CarpeDiem0110@users.noreply.github.com>
2025-08-19 17:04:18 +0300

    repository file is created

---
+++ backend/ProductAPI/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+using ProductAPI.Data;
+
+namespace ProductAPI.Repositories;
+
+public class ProductRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Product>> GetAllAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product> AddAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+}
+
